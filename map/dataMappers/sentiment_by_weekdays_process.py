4b36300724326aec1957ace77f64f4e37a093a57
GatoY <derakhy@gmail.com>
2018-05-08 20:11:03 +1000

    deploy

---
+++ map/dataMappers/sentiment_by_weekdays_process.py
@@ -0,0 +1,24 @@
+import json
+
+with open('../static/map/img/sentimentByWeekdays.json') as f:
+    data = json.load(f)
+    result = {"Mon": {"positive": 0, "negative": 0, "neutral": 0},
+              "Tue": {"positive": 0, "negative": 0, "neutral": 0},
+              "Wed": {"positive": 0, "negative": 0, "neutral": 0},
+              "Thu": {"positive": 0, "negative": 0, "neutral": 0},
+              "Fri": {"positive": 0, "negative": 0, "neutral": 0},
+              "Sat": {"positive": 0, "negative": 0, "neutral": 0},
+              "Sun": {"positive": 0, "negative": 0, "neutral": 0}}
+
+    entries = data["rows"]
+    for entry in entries:
+        result[entry["key"][0]][entry["key"][1]] += entry["value"]
+
+    for key in result:
+        percentage = result[key]["positive"] / (
+                result[key]["positive"] + result[key]["negative"] + result[key]["neutral"])
+        result[key]["percentage"] = round(percentage, 4)
+
+
+    with open('../static/map/res/sentiment_by_weekdays.json', 'w') as outFile:
+        json.dump(result, outFile)
--- map/views.py
+++ map/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect
 import couchdb
@@ -7,8 +8,9 @@ from django.http import HttpResponse
 # from couchdb import Server
 # from couchdb.client import ResourceNotFound
 
-#server = couchdb.Server('http://root:Couchdbmima@127.0.0.1:5984')
-#restResource = server['resource']
+#server = couchdb.Server('http://@127.0.0.1:5984')
+#restResource = server['map']
+
 
 def index(request):
     return render(request, 'map/index.html')
@@ -17,9 +19,23 @@ def index(request):
 def sentiment(request):
     return render(request, 'map/sentiment.html')
 
+# RESTful api
 #def sentimentData(request):
-#    sentiment_data = restResource["f331f3a656450464d3c8c9cbb800c5b1"]
+#    sentiment_data = restResource[""]
 #    return HttpResponse(json.dumps(sentiment_data), content_type='application/json')
+
+# RESTful api
+def sentiment_by_hours(request):
+    with open('map/static/map/res/sentiment_by_hours.json') as f:
+        data = json.load(f)
+        return HttpResponse(json.dumps(data), content_type='application/json')
+
+# RESTful api
+def sentiment_by_weekdays(request):
+    with open('map/static/map/res/sentiment_by_weekdays.json') as f:
+        data = json.load(f)
+        return HttpResponse(json.dumps(data), content_type='application/json')
+
 
 def scenario1(request):
     return render(request, 'map/scenario1.html')
@@ -29,8 +45,77 @@ def scenario2(request):
     return render(request, 'map/scenario2.html')
 
 
-def scenario3(request):
-    return render(request, 'map/scenario3.html')
+def affordability(request):
+    return render(request, 'map/affordability.html')
+
+
+def affordability_proportions(request):
+    with open('map/static/map/res/affordability_slim.json') as f:
+        data = json.load(f)
+        result = {
+            "rai_national_total_2017_q1": {"< 90": 0, "90 <= x < 110": 0, "110 <= x < 130": 0, "130 <= x < 150": 0,
+                                           ">= 150": 0, "N/A": 0},
+            "rai_national_total_2017_q2": {"< 90": 0, "90 <= x < 110": 0, "110 <= x < 130": 0, "130 <= x < 150": 0,
+                                           ">= 150": 0, "N/A": 0},
+            "rai_national_total_2016_q3": {"< 90": 0, "90 <= x < 110": 0, "110 <= x < 130": 0, "130 <= x < 150": 0,
+                                           ">= 150": 0, "N/A": 0},
+            "rai_national_total_2016_q4": {"< 90": 0, "90 <= x < 110": 0, "110 <= x < 130": 0, "130 <= x < 150": 0,
+                                           ">= 150": 0, "N/A": 0}}
+
+        for feature in data["features"]:
+            if feature["properties"]["rai_national_total_2017_q1"] is None:
+                result["rai_national_total_2017_q1"]["N/A"] += 1
+            elif feature["properties"]["rai_national_total_2017_q1"] < 90:
+                result["rai_national_total_2017_q1"]["< 90"] += 1
+            elif 90 <= feature["properties"]["rai_national_total_2017_q1"] < 110:
+                result["rai_national_total_2017_q1"]["90 <= x < 110"] += 1
+            elif 110 <= feature["properties"]["rai_national_total_2017_q1"] < 130:
+                result["rai_national_total_2017_q1"]["110 <= x < 130"] += 1
+            elif 130 <= feature["properties"]["rai_national_total_2017_q1"] < 150:
+                result["rai_national_total_2017_q1"]["130 <= x < 150"] += 1
+            else:
+                result["rai_national_total_2017_q1"][">= 150"] += 1
+
+            if feature["properties"]["rai_national_total_2017_q2"] is None:
+                result["rai_national_total_2017_q2"]["N/A"] += 1
+            elif feature["properties"]["rai_national_total_2017_q2"] < 90:
+                result["rai_national_total_2017_q2"]["< 90"] += 1
+            elif 90 <= feature["properties"]["rai_national_total_2017_q2"] < 110:
+                result["rai_national_total_2017_q2"]["90 <= x < 110"] += 1
+            elif 110 <= feature["properties"]["rai_national_total_2017_q2"] < 130:
+                result["rai_national_total_2017_q2"]["110 <= x < 130"] += 1
+            elif 130 <= feature["properties"]["rai_national_total_2017_q2"] < 150:
+                result["rai_national_total_2017_q2"]["130 <= x < 150"] += 1
+            else:
+                result["rai_national_total_2017_q2"][">= 150"] += 1
+
+            if feature["properties"]["rai_national_total_2016_q3"] is None:
+                result["rai_national_total_2016_q3"]["N/A"] += 1
+            elif feature["properties"]["rai_national_total_2016_q3"] < 90:
+                result["rai_national_total_2016_q3"]["< 90"] += 1
+            elif 90 <= feature["properties"]["rai_national_total_2016_q3"] < 110:
+                result["rai_national_total_2016_q3"]["90 <= x < 110"] += 1
+            elif 110 <= feature["properties"]["rai_national_total_2016_q3"] < 130:
+                result["rai_national_total_2016_q3"]["110 <= x < 130"] += 1
+            elif 130 <= feature["properties"]["rai_national_total_2016_q3"] < 150:
+                result["rai_national_total_2016_q3"]["130 <= x < 150"] += 1
+            else:
+                result["rai_national_total_2016_q3"][">= 150"] += 1
+
+            if feature["properties"]["rai_national_total_2016_q4"] is None:
+                result["rai_national_total_2016_q4"]["N/A"] += 1
+            elif feature["properties"]["rai_national_total_2016_q4"] < 90:
+                result["rai_national_total_2016_q4"]["< 90"] += 1
+            elif 90 <= feature["properties"]["rai_national_total_2016_q4"] < 110:
+                result["rai_national_total_2016_q4"]["90 <= x < 110"] += 1
+            elif 110 <= feature["properties"]["rai_national_total_2016_q4"] < 130:
+                result["rai_national_total_2016_q4"]["110 <= x < 130"] += 1
+            elif 130 <= feature["properties"]["rai_national_total_2016_q4"] < 150:
+                result["rai_national_total_2016_q4"]["130 <= x < 150"] += 1
+            else:
+                result["rai_national_total_2016_q4"][">= 150"] += 1
+
+        return HttpResponse(json.dumps(result), content_type='application/json')
 
 
 def aboutUs(request):
